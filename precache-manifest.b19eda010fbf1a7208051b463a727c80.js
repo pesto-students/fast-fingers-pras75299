25180999d03f87bfebd10399293ee7b252d5eaa5
pras75299 <singh.prashantking@gmail.com>
2020-10-06 18:54:30 +0530

    Updates

---
+++ precache-manifest.b19eda010fbf1a7208051b463a727c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22d302e8c5150eedf09dfd32a08441d",
+    "url": "/fast-fingers-pras75299/index.html"
+  },
+  {
+    "revision": "a002779b16cbd697dd12",
+    "url": "/fast-fingers-pras75299/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a71f263bb1682cbed32e",
+    "url": "/fast-fingers-pras75299/static/js/2.ca6aba6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fast-fingers-pras75299/static/js/2.ca6aba6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a002779b16cbd697dd12",
+    "url": "/fast-fingers-pras75299/static/js/main.a67a7749.chunk.js"
+  },
+  {
+    "revision": "f8293876350be449dd39",
+    "url": "/fast-fingers-pras75299/static/js/runtime-main.b27a8560.js"
+  }
+]);
